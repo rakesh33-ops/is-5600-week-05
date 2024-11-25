420eb05a4d3ff3869c2c82d223967fcb4bebbe97
rakesh33-ops <sairakesh505@gmail.com>
2024-11-25 05:48:11 +0000

    this is my lab 5

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const Products = require('./products')
+const Orders = require('./orders')
 const autoCatch = require('./lib/auto-catch')
 
 /**
@@ -10,7 +11,6 @@ const autoCatch = require('./lib/auto-catch')
 function handleRoot(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 }
-
 /**
  * List all products
  * @param {object} req
@@ -26,8 +26,6 @@ async function listProducts(req, res) {
     tag
   }))
 }
-
-
 /**
  * Get a single product
  * @param {object} req
@@ -35,15 +33,12 @@ async function listProducts(req, res) {
  */
 async function getProduct(req, res, next) {
   const { id } = req.params
-
   const product = await Products.get(id)
   if (!product) {
     return next()
   }
-
   return res.json(product)
 }
-
 /**
  * Create a product
  * @param {object} req 
@@ -52,6 +47,8 @@ async function getProduct(req, res, next) {
 async function createProduct(req, res) {
   console.log('request body:', req.body)
   res.json(req.body)
+  const product = await Products.create(req.body)
+  res.json(product)
 }
 
 /**
@@ -63,6 +60,12 @@ async function createProduct(req, res) {
 async function editProduct(req, res, next) {
   console.log(req.body)
   res.json(req.body)
+
+async function editProduct (req, res, next) {
+  const change = req.body
+  const product = await Products.edit(req.params.id, change)
+
+  res.json(product)
 }
 
 /**
@@ -73,7 +76,48 @@ async function editProduct(req, res, next) {
  */
 async function deleteProduct(req, res, next) {
   res.json({ success: true })
+async function deleteProduct (req, res, next) {
+  const response = await Products.destroy(req.params.id)
+
+  res.json(response)
 }
+
+async function createOrder (req, res, next) {
+  const order = await Orders.create(req.body)
+
+  res.json(orders)
+}
+
+async function editOrder(req, res, next) {
+  try {
+    const updatedOrder = await Orders.edit(req.params.id, req.body); // Calls the `edit` method from Orders
+    res.json(updatedOrder); // Sends the updated order as JSON
+  } catch (error) {
+    next(error); // Passes errors to the error-handling middleware
+  }
+}
+
+async function deleteOrder(req, res, next) {
+  try {
+    await Orders.destroy(req.params.id); // Calls the `destroy` method from Orders
+    res.status(204).send(); // Sends a no-content response
+  } catch (error) {
+    next(error); // Passes errors to the error-handling middleware
+  }
+}
+
+async function listOrders (req, res, next) {
+  const { offset = 0, limit = 25, productId, status } = req.query
+
+  const orders = await Orders.list({ 
+    offset: Number(offset), 
+    limit: Number(limit),
+    productId, 
+    status 
+  })
+
+  res.json(orders)
+} 
 
 module.exports = autoCatch({
   handleRoot,
@@ -81,5 +125,9 @@ module.exports = autoCatch({
   getProduct,
   createProduct,
   editProduct,
-  deleteProduct
+  deleteProduct,
+  listOrders,
+  createOrder,
+  editOrder,
+  deleteOrder
 });
--- products.js
+++ products.js
@@ -1,7 +1,36 @@
 const fs = require('fs').promises
 const path = require('path')
+const cuid = require('cuid')
+const db = require('./db')
 
 const productsFile = path.join(__dirname, 'data/full-products.json')
+
+// Define our Product Model
+const Product = db.model('Product', {
+  _id: { type: String, default: cuid },
+  description: { type: String },
+  alt_description: { type: String },
+  likes: { type: Number, required: true },
+  urls: {
+    regular: { type: String, required: true },
+    small: { type: String, required: true },
+    thumb: { type: String, required: true },
+  },
+  links: {
+    self: { type: String, required: true },
+    html: { type: String, required: true },
+  },
+  user: {
+    id: { type: String, required: true },
+    first_name: { type: String, required: true },
+    last_name: { type: String },
+    portfolio_url: { type: String },
+    username: { type: String, required: true },
+  },
+  tags: [{
+    title: { type: String, required: true },
+  }], 
+})
 
 /**
  * List products
@@ -17,7 +46,20 @@ async function list(options = {}) {
     .filter(product => {
       if (!tag) {
         return product
+  const query = tag ? {
+    tags: {
+      $elemMatch: {
+        title: tag
       }
+    }
+  } : {}
+
+  const products = await Product.find(query)
+    .sort({ _id: 1 })
+    .skip(offset)
+    .limit(limit)
+
+  return products
 
       return product.tags.find(({ title }) => title == tag)
     })
@@ -31,6 +73,10 @@ async function list(options = {}) {
  */
 async function get(id) {
   const products = JSON.parse(await fs.readFile(productsFile))
+async function get(_id) {
+  const product = await Product.findById(_id)
+  return product
+}
 
   // Loop through the products and return the product with the matching id
   for (let i = 0; i < products.length; i++) {
@@ -38,12 +84,37 @@ async function get(id) {
       return products[i]
     }
   }
+async function create (fields) {
+  const product = await new Product(fields).save()
+
+  return product
+}
+
+async function edit (_id, change) {
+  const product = await get(_id)
+
+  Object.keys(change).forEach(function (key) {
+    product[key] = change[key]
+  })
+
+  // Object.assign(product, { ...change } );
+
+  await product.save()
+
+  return product
+}
 
   // If no product is found, return null
   return null;
+async function destroy (_id) {
+  return await Product.deleteOne({_id})
 }
 
 module.exports = {
   list,
   get
+  get,
+  create,
+  edit,
+  destroy
 }
